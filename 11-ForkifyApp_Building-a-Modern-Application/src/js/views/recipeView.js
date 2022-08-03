8dfc80fd88669d916913053fd4a6c27914c5ab3d
pkErbynn <john.erbynn@gmail.com>
2022-08-03 09:06:11 +0000

    refactor to MVC architecture

--- 11-ForkifyApp_Building-a-Modern-Application/src/js/views/recipeView.js
+++ 11-ForkifyApp_Building-a-Modern-Application/src/js/views/recipeView.js
@@ -123,12 +123,12 @@ class RecipeView {
         return `
             <li class="recipe__ingredient">
                 <svg class="recipe__icon">
-                <use href="${icons}#icon-check"></use>
+                    <use href="${icons}#icon-check"></use>
                 </svg>
                 <div class="recipe__quantity">${ingredient.quantity ? new Fraction(ingredient.quantity).toString() : ''}</div>
                 <div class="recipe__description">
-                <span class="recipe__unit">${ingredient.unit}</span>
-                ${ingredient.description}
+                    <span class="recipe__unit">${ingredient.unit}</span>
+                    ${ingredient.description}
                 </div>
             </li>
             `
