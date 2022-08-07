10f1b9f1fbbed509451d6e1083d332dbb50d2b53
pkErbynn <john.erbynn@gmail.com>
2022-08-07 13:21:01 +0000

    clear search input field after search

--- 11-ForkifyApp_Building-a-Modern-Application/src/js/views/searchView.js
+++ 11-ForkifyApp_Building-a-Modern-Application/src/js/views/searchView.js
@@ -2,7 +2,9 @@ class SearchView {
     #parentEl = document.querySelector('.search');
 
     getQuery() {
-        return this.#parentEl.querySelector('.search__field').value;
+        const query = this.#parentEl.querySelector('.search__field').value;
+        this.#clearInput();
+        return query;
     }
 
     addSearchHandler(handler){
@@ -10,6 +12,10 @@ class SearchView {
             e.preventDefault(); // prevent page reload
             handler();
         })
+    }
+
+    #clearInput(){
+        this.#parentEl.querySelector('.search__field').value = "";
     }
 }
 
